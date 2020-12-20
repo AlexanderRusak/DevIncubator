28c0b73bffa0fc44274d1ead72dc9fda0196c191
Aliaksandr Rusak <rusak.alexander2017@gmail.com>
2020-12-20 22:55:59 +0300

    complete 3 HW and add English

--- Book/SelectionSort.cpp
+++ Book/SelectionSort.cpp
@@ -8,45 +8,34 @@ void selectionSort(int arr[],int sz);
 int main(){
 	const int SZ=7;
     int numbers[SZ]={7,0,-4,3,1,-2,5};
-    //cout<<getSmllestIndexOfArr(numbers,SZ);
-    selectionSort(numbers,SZ);
+    getSmllestIndexOfArr(numbers,SZ);
+    //selectionSort(numbers,SZ);
     return 0;
 }
 
 
 int getSmllestIndexOfArr(int num[],int sz){
-	int smallest=num[1];
-	for(int i=1; i < sz; i++){
-		if(num[i]<smallest){
-			smallest=num[i];
+	int smallest;
+	int smallestIndex;
+	for(int i=0;i<sz; i++){
+		smallest=num[i];
+		smallestIndex=i;
+		for(int j=i+1; j < sz; j++){
+			if(num[j]<smallest){
+				smallest=num[j];
+				smallestIndex=j;
+				}	
 		}
+		num[smallestIndex]=num[i];
+		num[i]=smallest;
 	}
+   
+  for(int i=0;i<sz;i++){
+   	cout<<num[i]<<endl;
+   }
+
+	
 	return smallest;
 }  
 
-void selectionSort(int arr[],int sz){
-	
-	int prev;
-	int buf;
-	int bufIndex;
-	int compareNumber;
-    for(int i=1;i<sz;i++){
-    	for(int j=1; j<sz; j++){
-    		if(arr[j-1]>arr[j]){  
-    			buf=arr[j];          
-    			bufIndex=j;         
-    		}
-    		cout<<buf<<"<-buf bufIndex->"<<bufIndex<<endl;
-    		cout<<"next "<<i<<endl;
-    		arr[bufIndex]=arr[j-1];
-    		arr[j-1]=buf;
-    		cout<<buf<<"<-buf bufIndex->"<<bufIndex<<endl;
-    	}
-
-    	
-    }
-    for(int i=0;i<sz;i++){
-    	cout<<arr[i]<<endl;
-    }
-}  
 
--- HW3/HW3 Task1 Fibonaссi.cpp
+++ HW3/HW3 Task1 Fibonaссi.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int getMemberOfFibonacciSeries(int n);
 void showSumOfFibonacciSeries(int n);
 int getPositiveNumber(int number);
+void showSumMessage(int number, int sum);
+void showMessage(int number, int value);
 
 int main()
 {
 
     int number;
-    
+
     number = getPositiveNumber(number);
     showSumOfFibonacciSeries(number);
     return 0;
@@ -19,14 +21,14 @@ int main()
 int getMemberOfFibonacciSeries(int n)
 {
     int bufferNumber, fibonacciNmuber = 0, prevFibonacciNumber = 1;
-    
+
     if (n <= 1)
     {
-        cout << "Fibinacci F(" << n << ") = " << fibonacciNmuber << endl; ///!!!!!!!!!!
-        
+        showMessage(n, fibonacciNmuber);
+
         return fibonacciNmuber;
     }
-    
+
     for (int i = 1; i < n; i++)
     {
         if (n == 2)
@@ -40,17 +42,17 @@ int getMemberOfFibonacciSeries(int n)
             prevFibonacciNumber = bufferNumber;
         }
     }
-    cout << "Fibinacci F(" << n << ") = " << fibonacciNmuber << endl;   // !!!!!!
-    
+    showMessage(n, fibonacciNmuber);
+
     return fibonacciNmuber;
 }
 
-int getPositiveNumber(int number)   ///Refact
+int getPositiveNumber(int number) ///Refact
 {
     cout << "Input number: ";
     cin >> number;
-    
-    while (!cin >> number || number <= 0)   ////!!!!!!!!!
+
+    while (!cin >> number || number <= 0) ////!!!!!!!!!
     {
         cout << "Input error!" << endl;
         cout << "Input number > 0: ";
@@ -69,6 +71,13 @@ void showSumOfFibonacciSeries(int n)
     {
         sumOfFibinacciSeries += getMemberOfFibonacciSeries(i);
     }
-    
-    cout << "Sum of Fibanacci series F(" << n << ") = " << sumOfFibinacciSeries << endl;   /// show  
+    showSumMessage(n, sumOfFibinacciSeries);
+}
+void showSumMessage(int number, int sum)
+{
+    cout << "Sum of Fibanacci series F(" << number << ") = " << sum << endl;
+}
+void showMessage(int number, int value)
+{
+    cout << "Fibinacci F(" << number << ") = " << value << endl;
 }
