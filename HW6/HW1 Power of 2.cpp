160b1f107602dcf16279cdb8b2eb45009b02e6b0
AlexanderRusak <37529388602@mail.ru>
2021-01-22 11:39:38 +0300

    task 1 about powers and cubes

--- HW6/HW1 Power of 2.cpp
+++ HW6/HW1 Power of 2.cpp
@@ -1,34 +1,48 @@
 #include <iostream>
 #include <math.h>
 #include <cstdlib>
-#define LIMIT 100000
-#define SIZE 5
+#define LIMIT 10000
+#define SIZE 10
 using namespace std;
   
-bool isPowerNumber(int number);
-void showNumbers(int array[]);
+bool isPowerNumber(int number, int comparingNumber);
+void showNumbers(int array[],int numbers);
+int exponintationInPositivePower(int number,int power);
 int main(){
-    int numbers[SIZE]={-4,2,64,4,5};
-    showNumbers(numbers);
+    int numbers[SIZE]={2,4,8,1,5,16,32,64,2,-5};
+    showNumbers(numbers,2);
+    
 	return 0;
 }
 
 
 
-bool isPowerNumber(int number){
+bool isPowerNumber(int number,int comparingNumber){
 	bool result=false;
 	for(int i = 0; i<=LIMIT; i++){
-		if(number==i*i){
+		if(comparingNumber==pow(number,i)){
+		
 			return !result;
 		}
 	}
 	return result;
 	
 }
-void showNumbers(int array[]){
+void showNumbers(int array[],int numbers){
 	for(int i=0; i < SIZE; i++){
-		if(isPowerNumber(array[i])){
+		if(isPowerNumber(numbers,array[i])){
 			cout<<array[i]<<" ";
 		}
 	}
+}
+
+int exponintationInPositivePower(int number,int power){
+	int result=1;
+	if(power==0){
+		return 1;
+	}
+	for(int i=0; i<power ;i++){
+		result*=number;
+	}
+	return result;
 }
--- testcpp.cpp
+++ testcpp.cpp
@@ -1,25 +1,51 @@
 #include <iostream>
+using namespace std;
+#define N 128
+bool isSeparator(char c);
 
-using namespace std; 
-
-
-  
-int main() 
+int main()
 {
-	int n=10;
-	for(int i=0;i<n;i++){
-		for(int j=0 ;j<i+1;j++){
-			cout<<"x";	
-		}
-		cout<<endl;
-	}
-	for(int i=n;i>0;i--){
-		for(int j=0 ;j<i-1;j++){
-			cout<<"x";	
-		}
-		cout<<endl;
-	}
-	
-    return 0; 
+    char text[N], word[N], maxWord[N];
+    int i, iw = 0, wordAmmount=0, wordLength=0, maxLength=0, startIndex=0;
+
+    cout << "Input text: ";
+    cin.getline(text, N);
+
+    for (i = 0; text[i] != '\0'; i++) {
+        if (!isSeparator(text[i])) {
+            word[iw] = text[i];
+            iw++;
+
+            
+            if (isSeparator(text[i+1]) or text[i+1] == '\0') {
+            	wordLength=iw;
+            	if(maxLength < wordLength){
+            		maxLength = wordLength;  
+					startIndex=i+1-maxLength;        		
+            	}
+                word[iw] = '\0';
+                cout << word << endl;
+                iw = 0;
+                wordAmmount++;
+                wordLength=0;
+            }
+        }
+    }
+    cout<<wordAmmount<<endl;
+    
+    for(int i = startIndex; i < maxLength+startIndex; i++){
+    	cout << text[i];
+    }
+    return 0;
 }
+
+bool isSeparator(char c)
+{
+    char s[] = ",.! ?";
+    for (int i = 0; s[i] != '\0'; i++) 
+        if (c == s[i])
+            return true;
+    return false;
+}
+
 
